63662836b386017dcac0dcbc02c9197192e5bada
Matthew Tung <tungm@usc.edu>
2023-02-11 01:08:08 -0800

    fix counter query

--- models/Counter.js
+++ models/Counter.js
@@ -134,9 +134,9 @@ async function getCounterRange(startRange, endRange, user_id) {
     a.color_id, a.archived, a.public, a.cur_count, b.date_key, b.daily_count, 1 as entry_type \
     FROM counter a, \
     (select date_key, sum(update_by) as daily_count, counter_id from counter_tally \
+    WHERE time_start >= $2 AND time_start < $3 AND daily_count > 0\
     group by date_key, counter_id) b \
-    WHERE a.counter_id = b.counter_id AND a.user_id = $1 AND b.time_start >= $2 AND b.time_start < $3 \
-    AND b.daily_count > 0'
+    WHERE a.user_id = $1 AND a.counter_id = b.counter_id'
 
     query_values = [user_id, startRange, endRange]
     const { rows } = await db.query(query_text_updated, query_values);
